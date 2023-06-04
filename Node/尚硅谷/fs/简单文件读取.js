ae5a7441169f94fa4975e28aac53fc306fdee04c
stahsf <50431040@qq.com>
2023-06-04 17:09:28 +0800

    feat: 删除一些文件

--- Node/尚硅谷/fs/简单文件读取.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-
-const file = path.join(__dirname, 'hello.txt')
-const data = fs.readFileSync(file)
-console.log(data.toString());
--- Node/尚硅谷/模块化/test.js
+++
@@ -1,4 +0,0 @@
-const obj = require('./模块化')
-
-console.log(obj); // Test
-console.log(a); // 123
